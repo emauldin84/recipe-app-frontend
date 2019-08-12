ead3b661139d4eaad09da830d1ec636127ecee86
Eric Mauldin <emauldin84@gmail.com>
2019-08-12 14:35:26 -0400

    adds BackButton component and displays in the recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,20 +1,28 @@
 import React from 'react'
 
+import BackButton from '../utils/BackButton'
+
 const Recipe = (props) => {
     const recipeData = props.selectedRecipe
+    console.log('RD',recipeData)
 
     let recipe = recipeData ? recipeData.map(recipe => {
         return(
             <div key={recipe.id}>
-                <h3 >{recipe.recipe_title}</h3>
-                <p>{recipe.recipe_added_date}</p>
-                <p>{recipe.recipe_details}</p>
+                
+                <div>
+                    <h3 >{recipe.recipe_title}</h3>
+                    <p>{recipe.recipe_added_date}</p>
+                    <p>{recipe.recipe_details}</p>
+                </div>
+
             </div>
         )
     }) : null
 
     return(
         <div>
+            <BackButton />
             {recipe}
         </div>
     )
---
+++ src/utils/BackButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let BackButton = (props) => {
+    return (
+        <div>
+            <div className="arrow arrow--left"></div>
+        </div>
+    )
+}
+
+export default BackButton
