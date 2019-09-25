c5c7261f3f5d379087e3b01946b73a7a27c00eed
Eric Mauldin <emauldin84@gmail.com>
2019-09-25 11:00:22 -0400

    removes broken image default display if no image url associated with recipe

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -4,12 +4,13 @@ const Recipes = (props) => {
     const recipesData = props.recipesData
 
     let recipes = recipesData ? recipesData.map(recipe => {
+        const image = recipe.recipe_photo ? <img src={recipe.recipe_photo} alt="Sample"/>  : null
         return(
             // <div key={recipe.id} onClick={() => props.handleClickedRecipe(recipe.id)}>
             //     <h3 >{recipe.recipe_title}</h3>
             // </div>
             <article className="card" key={recipe.id} onClick={() => props.handleClickedRecipe(recipe.id)}>
-                <img src={recipe.recipe_photo ? recipe.recipe_photo : null} alt="Sample"/>
+                {image}
                 <div className="recipeTitle">
                     <h3>{recipe.recipe_title}</h3>
                 </div>
