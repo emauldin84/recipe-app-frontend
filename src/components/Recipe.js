eaf678c0e91f1e5d6ae63fd21c3dfc3b2498e9a1
Eric Mauldin <emauldin84@gmail.com>
2019-09-26 17:13:03 -0400

    fixes issue where imageURLand receipe details were being set to null if saved without any changes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -44,8 +44,8 @@ class Recipe extends Component {
 
         const formData = {
             title: this.state.title,
-            details: this.state.detailsForUpload,
-            photo: this.state.image ? `public/uploads/IMAGE-${Date.now()}-${this.state.image.name}` : this.state.imageUrl ? this.state.imageUrl : null,
+            details: this.state.detailsForUpload !== null ? this.state.detailsForUpload : this.props.selectedRecipe[0].recipe_details,
+            photo: this.state.imageUrl !== null ? this.state.imageUrl : this.props.selectedRecipe[0].recipe_photo,
         }
         console.log('formData', formData)
 
@@ -57,7 +57,8 @@ class Recipe extends Component {
             this.setState({
                 editing: false,
             })
-            this.props.history.push('/')
+            // this.props.history.push(`/recipe/${this.props.selectedRecipe[0].id}`)
+            this.props.history.push(`/`)
 
         })
         .catch(err => err)
@@ -69,16 +70,16 @@ class Recipe extends Component {
             title: e.target.textContent,
         })
     }
-    // onImageUrlChangeHandler = (e) => {
-    //     this.setState({
-    //         imageUrl: e.target.value,
-    //         })
-    // }
+    onImageUrlChangeHandler = (e) => {
+        this.setState({
+            imageUrl: e.target.value,
+            })
+    }
 
     render() {
         // console.log('editedDetails',this.props.editedDetails)
         const recipeData = this.props.selectedRecipe
-        // console.log('RD',recipeData)
+        console.log('RD',recipeData)
 
         
         let recipe = recipeData ? recipeData.map(recipe => {
@@ -90,7 +91,7 @@ class Recipe extends Component {
                                                     editorState={EditorState.createWithContent(convertFromRaw(JSON.parse(recipe.recipe_details)))}
                                                     readOnly={true}
                                                 />
-            const image = recipe.recipe_photo ? <img className='recipeImage'src={recipe.recipe_photo} alt="Sample"/>  : null
+            const image = recipe.recipe_photo ? this.state.editing ? <input id='imageUrl' defaultValue={recipe.recipe_photo} className='Input imageUrlInput' onChange={this.onImageUrlChangeHandler}/> : <img className='recipeImage'src={recipe.recipe_photo} alt="Sample"/>  : null
             // console.log('RECIPE DETAILS', recipe.recipe_details)
             return(
                 <div key={recipe.id}>
